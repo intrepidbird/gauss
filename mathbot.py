40a8c7340b8e34de9be20170bdb5ed3e8059f6ab
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-09-15 15:56:29 -0400

    Update mathbot.py

--- mathbot.py
+++ mathbot.py
@@ -4,12 +4,16 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from discord import Intents
-
+from flask import Flask
+from threading import Thread app = Flask('') @app.route('/')
+def main(): return "Your Bot Is Ready" def run(): app.run(host="0.0.0.0", port=8000) def keep_alive(): server = Thread(target=run) server.start()
 intents = Intents.default()
 intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-
+status = cycle(['with Python','JetHub']) @bot.event
+async def on_ready(): change_status.start() print("Your bot is ready") @tasks.loop(seconds=10)
+async def change_status(): await bot.change_presence(activity=discord.Game(next(status)))
 
 @bot.command(name='calculate')
 async def calculate(ctx, *, expression: str):
