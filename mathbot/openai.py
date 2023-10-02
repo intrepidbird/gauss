23202e6fb81fba7709d2aa6e5fb4a914d43055af
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-10-02 08:44:03 -0400

    Update openai.py

--- mathbot/openai.py
+++ mathbot/openai.py
@@ -5,3 +5,5 @@ async def ai(ctx, *, prompt):
     openai.api_key = 'openai_key'
     response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100)
     await ctx.send(response.choices[0].text.strip())
+
+# OpenAI API
