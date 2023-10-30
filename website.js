9154a9d3d323b4d08286f3070dbf8875bd0ea4ec
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-10-29 21:08:01 -0400

    Create website.js

---
+++ website.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
+        alert('Thank you for contacting us! We will get back to you soon.');
+    });
+});
